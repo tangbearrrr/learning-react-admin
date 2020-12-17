043a82c0194bb3af2b6314e67342c74db12539dd
tangbearrrr <rh.sarawasee@gmail.com>
2020-12-17 22:27:03 +0700

    add list users component

---
+++ src/users.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <TextField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
